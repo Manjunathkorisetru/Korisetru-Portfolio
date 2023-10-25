89f9d1fb1762f0e6283717f511fa28f69558c1e8
Manjunath Korisetru <40051850+Manjunathkorisetru@users.noreply.github.com>
2023-10-25 22:22:45 +0200

    Fixed Contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,16 +1,24 @@
 "use client";
 import React from "react";
 import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
-function contact() {
+function Contact() {
   const { ref } = useSectionInView("Contact", 0.5);
   return (
-    <section ref={ref} id="contact" className="scroll-mt-28">
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
       <SectionHeading>Contact</SectionHeading>
       <p className="w-30 h-16 animate-blink">Work in Progress...</p>
-    </section>
+    </motion.section>
   );
 }
 
-export default contact;
+export default Contact;
