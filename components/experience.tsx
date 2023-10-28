0d99454a303dd4eb16043c9a4ea7f9dc04f5b7d6
Manjunath Korisetru <40051850+Manjunathkorisetru@users.noreply.github.com>
2023-10-28 15:09:14 +0200

    uninstalled react-vertical-timeline-component

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,11 +2,6 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-//import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 
@@ -16,41 +11,25 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="#000000">
+      <div>
         {experiencesData.map((experience, index) => {
           return (
             <React.Fragment key={index}>
-              <VerticalTimelineElement
-                //className="vertical-timeline-element--work"
-                className="max-w-2xl flex flex-col justify-center gap-4"
-                contentStyle={{
-                  background: "rgba(255, 255, 255, 0.05)",
-                  color: "#000000",
-                  boxShadow: "none",
-                  border: "1px solid rgba(0, 0, 0, 0.05)",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-                date={experience.date}
-                //icon={experience.icon}
-                iconStyle={{
-                  background: "white",
-                  fontSize: "1.5rem",
-                }}
-                contentArrowStyle={{
-                  borderRight: "0.4rem solid #9ca3af",
-                }}
+              <div
+                className="max-w-2xl flex flex-col justify-center gap-6 rounded-xl 
+              shadow-xl bg-slate-200 border-2 mb-20 p-6 sm:p-8"
               >
                 <h3 className="font-semibold capitalize">{experience.title}</h3>
                 <p className="font-normal !mt-0">{experience.location}</p>
                 <p className="!mt-1 !font-normal text-gray-700 text-wrap">
                   {experience.description}
                 </p>
-              </VerticalTimelineElement>
+                <p>{experience.date}</p>
+              </div>
             </React.Fragment>
           );
         })}
-      </VerticalTimeline>
+      </div>
     </section>
   );
 }
