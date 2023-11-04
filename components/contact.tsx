9aca0e53c95ac8c16439a0b2eaa29ce871c710b0
Manjunath Korisetru <40051850+Manjunathkorisetru@users.noreply.github.com>
2023-11-04 14:08:34 +0100

    Added Contact Form
    - added resend API package
    - added @react-email/components and @react-email/tailwindcss

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,24 +1,76 @@
 "use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
-function Contact() {
-  const { ref } = useSectionInView("Contact", 0.5);
+export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
     <motion.section
-      ref={ref}
       id="contact"
-      className="scroll-mt-28"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
-      <SectionHeading>Contact</SectionHeading>
-      <p className="w-30 h-16 animate-blink">Work in Progress...</p>
+      <SectionHeading>Contact me</SectionHeading>
+
+      <p className="text-gray-700 -mt-6 ">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:manjunathkori1993@gmail.com">
+          manjunathkori1993@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
+
+      <form
+        className="mt-10 flex flex-col text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack 
+          transition-all "
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 
+          transition-all"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
     </motion.section>
   );
 }
-
-export default Contact;
